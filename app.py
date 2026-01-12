abd6aa8770edadaa3cc4ac07eca14ea01a70cce3
Sivabalaji <sivabalaji@Sivabalajis-MacBook-Air.local>
2026-01-12 16:17:35 +0530

    Trigger CI/CD pipeline test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 DevOps Live Project is Running Successfully!"
+     return "🚀 CI/CD Pipeline is Working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
